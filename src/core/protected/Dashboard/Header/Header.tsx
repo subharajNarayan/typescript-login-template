8272df19bdb4cdec62974508b479bc50331e6011
Subharaj Chaudhary <subharazzchy68@gmail.com>
2023-05-04 16:08:41 +0545

    update header userDetails while refresh solved

--- src/core/protected/Dashboard/Header/Header.tsx
+++ src/core/protected/Dashboard/Header/Header.tsx
@@ -1,10 +1,11 @@
-import React,{ useState } from 'react';
+import React, { useState } from 'react';
 import hamburger from '../../../../assets/images/hamburger.png';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState, logoutAction } from '../../../../store/root-reducer';
 import { Dropdown, DropdownMenu, DropdownToggle, DropdownItem } from 'reactstrap';
 import { toast } from 'react-toastify';
 import TokenService from '../../../../services/jwt-token/jwt-token';
+import useAuthentication from '../../../../services/authentication/AuthService';
 
 interface Props {
   sidebarToggle: boolean;
@@ -15,6 +16,8 @@ const AppHeader = (props: Props) => {
   const { sidebarToggle, setsidebarToggle } = props;
 
   const dispatch = useDispatch();
+  const { isAuthenticated } = useAuthentication();
+
   const LogOutAction = () => {
     dispatch(logoutAction())
     toast.success('LogOut Successful !!!')
@@ -26,14 +29,14 @@ const AppHeader = (props: Props) => {
 
 
   const userDetails = TokenService.getAccessToken();
-  
+
 
   const togglesidebar = () => setsidebarToggle(!sidebarToggle);
   // console.log(sidebarToggle, "True");
 
   const [dropdownOpen, setDropdownOpen] = useState(false);
-  const toggle = () => setDropdownOpen((prevState) => !prevState);  
-  
+  const toggle = () => setDropdownOpen((prevState) => !prevState);
+
 
   return (
     <>
@@ -46,20 +49,23 @@ const AppHeader = (props: Props) => {
           </div>
           <div className='list list__inline list-separator px-4'>
             <div className='header-username'>
-            <Dropdown isOpen={dropdownOpen} toggle={toggle} tag="div">
-              <DropdownToggle className="auth" tag="div" role="button">
-                <div className="textbox mr-2">
-                  <h6 className="username font-bold">{userDetails.firstname}</h6>
-                </div>
-                <i className="ic-dropdown"></i>
-              </DropdownToggle>
-              <DropdownMenu>
-                <DropdownItem onClick={LogOutAction} className="dropdown-item text-danger">
-                  <i className="ic-logout"></i>
-                  Logout
-                </DropdownItem>
-              </DropdownMenu>
-            </Dropdown>
+              {isAuthenticated() && (
+
+                <Dropdown isOpen={dropdownOpen} toggle={toggle} tag="div">
+                  <DropdownToggle className="auth" tag="div" role="button">
+                    <div className="textbox mr-2">
+                      <h6 className="username font-bold">{userDetails.firstname}</h6>
+                    </div>
+                    <i className="ic-dropdown"></i>
+                  </DropdownToggle>
+                  <DropdownMenu>
+                    <DropdownItem onClick={LogOutAction} className="dropdown-item text-danger">
+                      <i className="ic-logout"></i>
+                      Logout
+                    </DropdownItem>
+                  </DropdownMenu>
+                </Dropdown>
+              )}
             </div>
           </div>
         </div>
